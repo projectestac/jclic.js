106f374ef1d6837e25244653d3f286accc60313f
Francesc Busquets <francesc@gmail.com>
2015-07-19 19:02:54 +0200

    Converting code comments to JSDoc format

--- src/activities/text/TextActivityDocument.js
+++ src/activities/text/TextActivityDocument.js
@@ -21,11 +21,220 @@ define([
   "../../boxes/ActiveBagContent"
 ], function ($, Utils, ActiveBoxContent, MediaContent, ActiveBagContent) {
 
-  //   // #### <a name="TextTarget">TextTarget</a> ####
-  // This class contains the properties and methods of the document elements that are the 
-  // real targets of user actions in text activities.
-  //
-  var TextTarget = function (doc, text) {
+  /**
+   * This is the HTML DOM element used in text activities like {@link FillInBlanks},
+   * {@link IdentifyText}, {@link OrderText} and {@link Complete}. It contains the main document of
+   * the activity, usually with some elements marked as "targets". In {@link FillInBlanks}, this
+   * targets are encapsulated in {@link TextActivityDocument.TextTarget} objects.
+   * @exports TextActivityDocument
+   * @class
+   */
+  var TextActivityDocument = function () {
+    // Make a deep clone of the default style
+    this.style = {'default': $.extend(true, {}, TextActivityDocument.DEFAULT_DOC_STYLE)};
+    this.p = [];
+    //this.tmb=new TargetMarkerBag();
+    this.boxesContent = new ActiveBagContent();
+    this.popupsContent = new ActiveBagContent();
+  };
+
+  TextActivityDocument.prototype = {
+    constructor: TextActivityDocument,
+    /**
+     * Number of blank spaces between tabulators.
+     * @type {number} */
+    tabSpc: 12,
+    /**
+     * Index of the last {@link ActiveBox} activated.
+     * @type {number} */
+    lastBoxId: 0,
+    /**
+     * A bag of {@link TargetMarker} objects
+     * @type {object} */
+    tmb: null,
+    /**
+     * Type of targets used in this activity. Possible values are: `TT_FREE`, `TT_CHAR`, `TT_WORD`
+     * and `TT_PARAGRAPH`.
+     * @type {string} */
+    targetType: 'TT_FREE',
+    /**
+     * Bag with the content of the boxes embedded in the document.
+     * @type {ActiveBagContent} */
+    boxesContent: null,
+    /**
+     * Bag with the content of the popups used by this activity.
+     * @type {ActiveBagContent} */
+    popupsContent: null,
+    /**
+     * Collection of named styles of the document
+     * @type {object} */
+    style: null,
+    /**
+     * The main document, represented as a collection of DOM objects
+     * @type {object} */
+    p: null,
+    /**
+     * 
+     * Loads the document settings from a specific JQuery XML element 
+     * @param {external:jQuery} $xml - The XML element to parse
+     * @param {MediaBag} mediaBag - The media bag used to load images and media content
+     */
+    setProperties: function ($xml, mediaBag) {
+
+      var doc = this;
+
+      // Read named styles
+      $xml.children('style').each(function () {
+        var attr = doc.readDocAttributes($(this));
+        doc.style[attr.name] = attr;
+      });
+
+      // Read paragraphs
+      $xml.find('section > p').each(function () {
+
+        var p = {elements: []};
+
+        // Read paragraph attributes
+        $.each(this.attributes, function () {
+          var name = this.name;
+          var value = this.value;
+          switch (this.name) {
+            case 'style':
+              p[name] = value;
+              break;
+            case 'bidiLevel':
+            case 'Alignment':
+              p[name] = Number(value);
+              break;
+          }
+        });
+
+        // Read paragraph objects
+        $(this).children().each(function () {
+          var obj;
+          var $child = $(this);
+          switch (this.nodeName) {
+
+            case 'cell':
+              obj = new ActiveBoxContent().setProperties($child, mediaBag);
+              break;
+
+            case 'text':
+              obj = {text: this.textContent.replace(/\t/g, '&#9;')};
+              var attr = doc.readDocAttributes($child);
+              if (!$.isEmptyObject(attr)) {
+                obj.attr = attr;
+              }
+              break;
+
+            case 'target':
+              obj = new TextActivityDocument.TextTarget(doc, this.textContent.replace(/\t/g, '&#9;'));
+              obj.setProperties($child, mediaBag);
+              break;
+
+            default:
+              console.log('[JClic] - Unknown object in activity document: ' + this.nodeName);
+          }
+          if (obj) {
+            obj.objectType = this.nodeName;
+            p.elements.push(obj);
+          }
+        });
+
+        doc.p.push(p);
+      });
+
+      return this;
+    },
+    /**
+     * 
+     * Reads sets of text attributes, sometimes in form of named styles
+     * @param {external:jQuery} $xml - The XML element to parse
+     * @returns {object}
+     */
+    readDocAttributes: function ($xml) {
+      var attr = {};
+      var css = {};
+      var thisDoc = this;
+      $.each($xml.get(0).attributes, function () {
+        var name = this.name;
+        var val = this.value;
+        switch (name) {
+          case 'background':
+            val = Utils.checkColor(val, 'white');
+            attr[name] = val;
+            css['background'] = val;
+            break;
+          case 'foreground':
+            val = Utils.checkColor(val, 'black');
+            attr[name] = val;
+            css['color'] = val;
+            break;
+          case 'family':
+            css['font-family'] = val;
+            /* falls through */
+          case 'name':
+          case 'base':
+          case 'style':
+            // Attributes specific to named styles:
+            attr[name] = val;
+            break;
+          case 'bold':
+            val = Utils.getBoolean(val);
+            attr[name] = val;
+            css['font-weight'] = val ? 'bold' : 'normal';
+            break;
+          case 'italic':
+            val = Utils.getBoolean(val);
+            attr[name] = val;
+            css['font-style'] = val ? 'italic' : 'normal';
+            break;
+          case 'target':
+            attr[name] = Utils.getBoolean(val);
+            break;
+          case 'size':
+            attr[name] = Number(val);
+            css['font-size'] = val + 'px';
+            break;
+          case 'tabWidth':
+            // `tab-size` CSS attribute is only set when the document has a specific `tabWidth`
+            // setting. It must be accompained of `white-space:pre` to successfully work.
+            thisDoc.tabSpc = val;
+            css['tab-size'] = thisDoc.tabSpc;
+            css['white-space'] = 'pre-wrap';
+            break;
+          default:
+            console.log('Unknown text attribute: ' + name + ': ' + val);
+            attr[name] = val;
+            break;
+        }
+      });
+
+      if (!$.isEmptyObject(css))
+        attr['css'] = css;
+
+      return attr;
+    }
+  };
+
+  /**
+   * Default style for new documents
+   * @type {object} */
+  TextActivityDocument.DEFAULT_DOC_STYLE = {background: 'white', foreground: 'black',
+    family: 'Arial', size: 17,
+    css: {'font-family': 'Arial,Helvetica,sans-serif', 'font-size': '17px',
+      'margin': '0px', padding: '0px', 'text-align': 'center', 'vertical-align': 'middle'}
+  };
+
+
+  /**
+   * This class contains the properties and methods of the document elements that are the real
+   * targets of user actions in text activities.
+   * @class
+   * @param {TextActivityDocument} doc - The document to which this target belongs.
+   * @param {string} text - Main text of this target.
+   */
+  TextActivityDocument.TextTarget = function (doc, text) {
     this.doc = doc;
     this.text = text;
     this.numIniChars = text.length;
@@ -33,83 +242,115 @@ define([
     this.maxLenResp = this.numIniChars;
   };
 
-  TextTarget.prototype = {
-    constructor: TextTarget,
-    // The TextActivityDocument to which this target belongs
+  TextActivityDocument.TextTarget.prototype = {
+    constructor: TextActivityDocument.TextTarget,
+    /**
+     * The TextActivityDocument to which this target belongs
+     * @type {TextActivityDocument} */
     doc: null,
-    //
-    // The current text displayed by this TextTarget
+    /**
+     * The current text displayed by this TextTarget
+     * @type {string} */
     text: null,
-    //
-    // A set of optional attributes for `text`
+    /**
+     * A set of optional attributes for `text`
+     * @type {object} */
     attr: null,
-    // 
-    // Target is a drop-down list
+    /**
+     * `true` when the target is a drop-down list
+     * @type {boolean} */
     isList: false,
-    //
-    // Number of characters initially displayed on the text field
+    /**
+     * Number of characters initially displayed on the text field
+     * @type {number} */
     numIniChars: 1,
-    //
-    // Character used to fill-in the text field
+    /**
+     * Character used to fill-in the text field
+     * @type {string} */
     iniChar: '_',
-    //
-    // Maximum length of the answer
+    /**
+     * Maximum length of the answer
+     * @type {number} */
     maxLenResp: 0,
-    //
-    // Array of valid answers
+    /**
+     * Array of valid answers
+     * @type {string[]} */
     answers: null,
-    //
-    // Array of specific options
+    /**
+     * Set of specific options
+     * @type {object} */
     options: null,
-    //
-    // Initial text
+    /**
+     * Text displayed by the target when the activity begins
+     * @type {string} */
     iniText: null,
-    //
-    // Type of additional information offered to the user. Valid values are:
-    // `no_info`, `always`, `onError`, `onDemand`
+    /**
+     * Type of additional information offered to the user. Possible values are: `no_info`, `always`,
+     * `onError` and `onDemand`.
+     * @type {string} */
     infoMode: 'no_info',
-    //
-    // An optional [ActiveBoxContent](ActiveBoxContent.html) with information about this TextTarget
+    /**
+     * An optional {@link ActiveBoxContent} with information about this TextTarget
+     * @type {ActiveBoxContent} */
     popupContent: null,
-    //
-    // Time to wait before showing the additional information
+    /**
+     * Time (seconds) to wait before showing the additional information
+     * @type {number} */
     popupDelay: 0,
-    //
-    // Maximum amount of time the additional inforation will be shown
+    /**
+     * Maximum amount of time (seconds) that the additional inforation will be shown
+     * @type {number} */
     popupMaxTime: 0,
-    //
-    // When this flag is `true` and `popupContent` contains audio, no visual feedback will be provided
-    // (the audio will be just played)
+    /**
+     * When this flag is `true` and `popupContent` contains audio, no visual feedback will be
+     * provided (meaning that audio will be just played)
+     * @type {boolean} */
     onlyPlay: false,
     //
     // TRANSIENT PROPERTIES
     //
-    // The drop-down list or span element associated to this target
+    /**
+     * The drop-down list associated to this target
+     * @type {external:jQuery} */
     $comboList: null,
+    /**
+     * The span element associated to this target
+     * @type {external:jQuery} */
     $span: null,
-    // 
-    // Current text in the $span element
+    /**
+     * Current text in the $span element
+     * @type {string} */
     currentText: '',
-    //
-    // Ordinal number of this target in the collection of targets
+    /**
+     * Ordinal number of this target in the collection of targets
+     * @type {number} */
     num: 0,
-    //
-    // Current target status. Valid values are: `NOT_EDITED`, `EDITED`, `SOLVED` and `WITH_ERROR`
+    /**
+     * Current status of the target. Valid values are: `NOT_EDITED`, `EDITED`, `SOLVED` and `WITH_ERROR`
+     * @type {string} */
     targetStatus: 'NOT_EDITED',
-    //
-    // Flag to control if the initial content of this TextTarget has been mofifed
+    /**
+     * Flag to control if the initial content of this TextTarget has been mofifed
+     * @type {boolean} */
     flagModified: false,
-    //
-    // Pointer to the TextActivityBase.Panel containing this TextTarget
+    /**
+     * Pointer to the activity panel containing this TextTarget
+     * @type {TextActivityBase.Panel} */
     parentPane: null,
-    //
-    // Resets the TextTarget status
+    /**
+     * 
+     * Resets the TextTarget status
+     */
     reset: function () {
       this.targetStatus = 'NOT_EDITED';
       this.flagModified = false;
     },
-    //
-    // Loads the object settings from a specific JQuery XML element 
+    /**
+     * 
+     * Loads the text target settings from a specific JQuery XML element 
+     * @param {external:jQuery} $xml - The XML element to parse
+     * @param {MediaBag} mediaBag - The media bag used to load images and media content
+     */
     setProperties: function ($xml, mediaBag) {
       var tt = this;
       var firstAnswer = true;
@@ -171,13 +412,19 @@ define([
         }
       });
     },
-    //
-    // Gets a string with all valid answers of this Texttarget. Useful for reporting users activity.
+    /**
+     * 
+     * Gets a string with all valid answers of this TextTarget. Useful for reporting users' activity.
+     * @returns {string}
+     */
     getAnswers: function () {
       return this.answers ? this.answers.join('|') : '';
     },
-    //
-    // Sets specific colors to the target JQuery element, based on its `targetStatus` value.
+    /**
+     * 
+     * Sets specific colors to the target jQuery element, based on its `targetStatus` value. Red
+     * color usually means error.
+     */
     checkColors: function () {
       var $element = this.$comboList ? this.$comboList : this.$span;
       if ($element) {
@@ -187,188 +434,6 @@ define([
         }
       }
     }
-  };
-
-  //
-  // TextActivityDocument is the main HTML document used in text activities
-  var TextActivityDocument = function () {
-    // Make a deep clone of the default style
-    this.style = {'default': $.extend(true, {}, this.DEFAULT_DOC_STYLE)};
-    this.p = [];
-    //this.tmb=new TargetMarkerBag();
-    this.boxesContent = new ActiveBagContent();
-    this.popupsContent = new ActiveBagContent();
-  };
-
-  TextActivityDocument.prototype = {
-    constructor: TextActivityDocument,
-    //
-    // Blank spaces between tabulators
-    tabSpc: 12,
-    //
-    // Last ActiveBox activated
-    lastBoxId: 0,
-    //
-    // A bag of TargetMarker objects
-    tmb: null,
-    //
-    // Type of targets used in this activity.
-    // Valid values are: `TT_FREE`, `TT_CHAR`, `TT_WORD`, `TT_PARAGRAPH`
-    targetType: 'TT_FREE',
-    //
-    // Two [ActiveBagContent](ActiveBagContent.html) objects with the content of boxes and pop-ups
-    boxesContent: null,
-    popupsContent: null,
-    //
-    // Collection of named styles of the document
-    style: null,
-    //
-    // The main document, represented as an array of DOM objects
-    p: null,
-    //
-    // Loads the object settings from a specific JQuery XML element 
-    setProperties: function ($xml, mediaBag) {
-
-      var doc = this;
-
-      // Read named styles
-      $xml.children('style').each(function () {
-        var attr = doc.readDocAttributes($(this));
-        doc.style[attr.name] = attr;
-      });
-
-      // Read paragraphs
-      $xml.find('section > p').each(function () {
-
-        var p = {elements: []};
-
-        // Read paragraph attributes
-        $.each(this.attributes, function () {
-          var name = this.name;
-          var value = this.value;
-          switch (this.name) {
-            case 'style':
-              p[name] = value;
-              break;
-            case 'bidiLevel':
-            case 'Alignment':
-              p[name] = Number(value);
-              break;
-          }
-        });
-
-        // Read paragraph objects
-        $(this).children().each(function () {
-          var obj;
-          var $child = $(this);
-          switch (this.nodeName) {
-
-            case 'cell':
-              obj = new ActiveBoxContent().setProperties($child, mediaBag);
-              break;
-
-            case 'text':
-              obj = {text: this.textContent.replace(/\t/g, '&#9;')};
-              var attr = doc.readDocAttributes($child);
-              if (!$.isEmptyObject(attr)) {
-                obj.attr = attr;
-              }
-              break;
-
-            case 'target':
-              obj = new TextTarget(doc, this.textContent.replace(/\t/g, '&#9;'));
-              obj.setProperties($child, mediaBag);
-              break;
-
-            default:
-              console.log('[JClic] - Unknown object in activity document: ' + this.nodeName);
-          }
-          if (obj) {
-            obj.objectType = this.nodeName;
-            p.elements.push(obj);
-          }
-        });
-
-        doc.p.push(p);
-      });
-
-      return this;
-    },
-    //
-    // Reads sets of text attributes, sometimes in form of named styles
-    readDocAttributes: function ($xml) {
-      var attr = {};
-      var css = {};
-      var thisDoc = this;
-      $.each($xml.get(0).attributes, function () {
-        var name = this.name;
-        var val = this.value;
-        switch (name) {
-          case 'background':
-            val = Utils.checkColor(val, 'white');
-            attr[name] = val;
-            css['background'] = val;
-            break;
-          case 'foreground':
-            val = Utils.checkColor(val, 'black');
-            attr[name] = val;
-            css['color'] = val;
-            break;
-          case 'family':
-            css['font-family'] = val;
-            /* falls through */            
-          case 'name':
-          case 'base':
-          case 'style':
-            // Attributes specific to named styles:
-            attr[name] = val;
-            break;
-          case 'bold':
-            val = Utils.getBoolean(val);
-            attr[name] = val;
-            css['font-weight'] = val ? 'bold' : 'normal';
-            break;
-          case 'italic':
-            val = Utils.getBoolean(val);
-            attr[name] = val;
-            css['font-style'] = val ? 'italic' : 'normal';
-            break;
-          case 'target':
-            attr[name] = Utils.getBoolean(val);
-            break;
-          case 'size':
-            attr[name] = Number(val);
-            css['font-size'] = val + 'px';
-            break;
-          case 'tabWidth':
-            // `tab-size` CSS attribute is only set when the document has a specific `tabWidth`
-            // setting. It must be accompained of `white-space:pre` to successfully work.
-            thisDoc.tabSpc = val;
-            css['tab-size'] = thisDoc.tabSpc;
-            css['white-space'] = 'pre-wrap';
-            break;
-          default:
-            console.log('Unknown text attribute: ' + name + ': ' + val);
-            attr[name] = val;
-            break;
-        }
-      });
-
-      if (!$.isEmptyObject(css))
-        attr['css'] = css;
-
-      return attr;
-    },
-    // 
-    // Default style for new documents
-    DEFAULT_DOC_STYLE: {background: 'white', foreground: 'black',
-      family: 'Arial', size: 17,
-      css: {'font-family': 'Arial,Helvetica,sans-serif', 'font-size': '17px',
-        'margin': '0px', padding: '0px', 'text-align': 'center', 'vertical-align': 'middle'}
-    },
-    // 
-    // A reference to the TextTarget class
-    TextTarget: TextTarget
   };
 
   return TextActivityDocument;
