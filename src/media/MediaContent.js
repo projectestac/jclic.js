8987900722a6a5760bd71c104a699a216496cad4
Francesc Busquets <francesc@gmail.com>
2016-03-04 21:59:00 +0100

    Solving bug in MediaContent.isEquivalent

--- src/media/MediaContent.js
+++ src/media/MediaContent.js
@@ -169,7 +169,9 @@ define([
      */
     isEquivalent: function (mc) {
       return this.mediaType === mc.mediaType &&
-          this.mediaFileName.toLocaleLowerCase() === mc.mediaFileName.toLocaleLowerCase() &&
+          (this.mediaFileName === mc.mediaFileName ||
+           (this.mediaFileName !== null && mc.mediaFileName !== null &&
+            this.mediaFileName.toLocaleLowerCase() === mc.mediaFileName.toLocaleLowerCase())) &&
           this.from === mc.from &&
           this.to === mc.to &&
           this.recBuffer === mc.recBuffer;
