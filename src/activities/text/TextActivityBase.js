180c68415518ba2f5809985cccd7bea041bb43c8
Francesc Busquets <francesc@gmail.com>
2016-04-24 17:23:39 +0200

    Partially reverting commit ba5330d that caused misfunctions in text activities

--- src/activities/text/TextActivityBase.js
+++ src/activities/text/TextActivityBase.js
@@ -365,25 +365,8 @@ define([
      * @returns {boolean=} - When this event handler returns `false`, jQuery will stop its		
      * propagation through the DOM tree. See: {@link http://api.jquery.com/on}		
      */
-    processEvent: function (event) {
-      if (this.playing) {
-        switch (event.type) {
-          case 'click':
-            var p = new AWT.Point(
-                event.pageX - this.$div.offset().left,
-                event.pageY - this.$div.offset().top);
-            for (var i = 0; i < this.boxes.length; i++) {
-              if (this.boxes[i].contains(p)) {
-                event.preventDefault();
-                this.ps.stopMedia(1);
-                this.boxes[i].playMedia(this.ps);
-                return false;
-              }
-            }
-        }
-        return true;
-      }
-      return false;
+    processEvent: function (event) {      
+      return this.playing;      
     }
   };
 
