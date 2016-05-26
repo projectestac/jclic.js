4c34398c8e8255ac6a7f90d11d180288242788db
Francesc Busquets <francesc@gmail.com>
2016-05-26 23:21:41 +0200

    Creating locale files for each language

---
+++ src/locales/Locales.js
@@ -0,0 +1,87 @@
+//  File    : Locales.js  
+//  Created : May 26, 2016  
+//  By      : linkat  
+//
+//  JClic.js  
+//  HTML5 player of [JClic](http://clic.xtec.cat) activities  
+//  http://projectestac.github.io/jclic.js  
+//  (c) 2000-2015 Catalan Educational Telematic Network (XTEC)  
+//  This program is free software: you can redistribute it and/or modify it under the terms of
+//  the GNU General Public License as published by the Free Software Foundation, version. This
+//  program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+//  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//  General Public License for more details. You should have received a copy of the GNU General
+//  Public License along with this program. If not, see [http://www.gnu.org/licenses/].  
+
+define([
+  'i18next',
+  './en',
+  './ca',
+  './es'
+], function (i18next, en, ca, es) {
+
+  var Locales = {
+    /**
+     * Checks if the language preferred by the user (based on browser and/or specific settings)
+     * is in a list of available languages.
+     * @param {string[]} availableLanguages - Array of available languages. It should contain at least one item.
+     * @param {string=} defaultLanguage -Language to be used by default when not found the selected one
+     * @param {string} setLang - Request this specific language
+     * @returns {string} - The most suitable language for this request
+     */
+    checkPreferredLanguage: function (availableLanguages, defaultLanguage, setLang) {
+      var result = -1;
+      // Create an array to store possible values
+      var tries = [];
+      // If "setLang" is specified, check it
+      if (setLang)
+        tries.push(setLang);
+
+      // Add user's preferred languages, if any
+      if (window.navigator.languages)
+        tries = tries.concat(window.navigator.languages);
+
+      // Add the navigator main language, if defined
+      if (window.navigator.language)
+        tries.push(window.navigator.language);
+
+      // Add English as final option
+      tries.push(defaultLanguage ? defaultLanguage : 'en');
+
+      for (var i in tries) {
+        for (var n in availableLanguages) {
+          if (tries[i].indexOf(availableLanguages[n]) === 0) {
+            result = n;
+            break;
+          }
+        }
+        if (result >= 0)
+          break;
+      }
+      return availableLanguages[result >= 0 ? result : 0];
+    },
+    /**
+     * Initializes i18next, assigning the translation function to ps
+     * @param {type} ps
+     */
+    init: function (ps) {
+      i18next.init({
+        fallbackLng: 'en',
+        lng: Locales.checkPreferredLanguage(['en', 'ca', 'es'], 'en', ps.lang),
+        resources: {
+          en: en,
+          ca: ca,
+          es: es
+        }
+      }, function (err, t) {
+        if (err)
+          console.log('Error initializing "i18next": ' + err);
+        else
+          ps.getMsg = t;
+      });
+    }
+  };
+
+  return Locales;
+
+});
---
+++ src/locales/es.js
@@ -0,0 +1,29 @@
+//
+// locales/es.js
+define([], function () {
+  return {translation: {
+      time: 'tiempo',
+      actions: 'acciones',
+      score: 'aciertos',
+      resultsNotSaved: 'Los resultados no se están guardando',
+      currentResults: 'Resultados actuales:',
+      sessionStarted: 'Inicio de sesión:',
+      reportsSystem: 'Sistema de informes:',
+      user: 'Usuario/aria:',
+      projects: 'Proyectos:',
+      sequences: 'Secuencias:',
+      activitiesDone: 'Actividades hechas:',
+      activitiesSolved: 'Actividades resueltas:',
+      globalScore: 'Puntuación global:',
+      totalTime: 'Tiempo total en las actividades:',
+      actionsDone: 'Acciones realizadas:',
+      noActivitiesDone: '¡No se ha realizado ninguna actividad!',
+      project: 'Proyecto',
+      sequence: 'secuencia',
+      activity: 'actividad',
+      solved: 'OK',
+      YES: 'SI',
+      NO: 'NO',
+      totals: 'Totales:'
+    }};
+});
