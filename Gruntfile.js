d39cb84bd4552ef26783be9abb34fca26aa3eda4
Francesc Busquets <francesc@gmail.com>
2016-06-01 17:02:35 +0200

    Add remaining translations

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,8 @@ module.exports = function (grunt) {
   var pkg = grunt.file.readJSON('package.json');
   
   // List of curently available languages and map of language '.po' files
-  var LANGUAGES = ['en', 'ca', 'es', 'ca_ES@valencia', 'ar', 'ast', 'bs', 'cs', 'da', 'de'];
+  var LANGUAGES = ['en', 'ar', 'ast', 'bs', 'ca', 'ca_ES@valencia', 'cs', 'da', 'de', 'el', 'es', 'eu', 'fr', 'gl', 'he',
+                   'it', 'nl', 'pt', 'pt_BR', 'ru', 'tr', 'vec', 'zh_TW'];
   var LNG_MAP = {};
   for(var p=1; p<LANGUAGES.length; p++)
     LNG_MAP[LANGUAGES[p]] = 'locales/' + LANGUAGES[p] + '.po';
@@ -20,13 +21,13 @@ module.exports = function (grunt) {
     //
     // Define metadata elements used in other tasks
     meta: {
-      banner:
-              '// JClic.js version <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>)\n' +
-              '// HTML5 player of JClic activities\n' +
-              '// (c) 2000-<%= grunt.template.today("yyyy") %> Educational Telematic Network of Catalonia (XTEC)\n' +
-              '// This program can be freely redistributed under the terms of the GNU General Public License\n' +
-              '// WARNING: You are reading a minimized, uglifyed version of jclic.js. Full, readable source\n' +
-              '// code is freely available at: <%= pkg.homepage %>\n'
+      banner: '\
+// JClic.js version <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>)\n\
+// HTML5 player of JClic activities\n\
+// (c) 2000-<%= grunt.template.today("yyyy") %> Educational Telematic Network of Catalonia (XTEC)\n\
+// This program can be freely redistributed under the terms of the GNU General Public License\n\
+// WARNING: You are reading a minimized, uglifyed version of jclic.js. Full, readable source\n\
+// code is freely available at: <%= pkg.homepage %>\n'
     },
     //
     // Check source code for possible inconsistencies or syntax errors
