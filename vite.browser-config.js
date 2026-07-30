69946b20714eb5abeb5258b45abed61823dd4d35
Francesc Busquets <francesc@gmail.com>
2026-07-30 18:39:50 +0200

    Update bundle banner

--- vite.browser-config.js
+++ vite.browser-config.js
@@ -10,29 +10,24 @@ ${pkg.name} v${version}
 ${pkg.description}
 ${pkg.homepage}
 
-MIT License
+(c) ${date.getFullYear()} ${pkg.author.name || pkg.author}
 
-Copyright (c) ${date.getFullYear()} ${pkg?.author.name || pkg?.author}
+Licensed under the EUPL, Version 1.2 or -as soon they will be approved by
+the European Commission- subsequent versions of the EUPL (the "Licence");
+You may not use this work except in compliance with the Licence.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the " Software"), to deal in
-the Software without restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
-Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
+You may obtain a copy of the Licence at:
+https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
 
-The above copyright notice and this permission notice (including the next paragraph)
-shall be included in all copies or substantial portions of the Software.
+Unless required by applicable law or agreed to in writing, software
+distributed under the Licence is distributed on an "AS IS" basis, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+Licence for the specific language governing permissions and limitations
+under the Licence.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+For full license information of included components please see: components.LICENSE
 
-WARNING: This is a compressed version of "${pkg?.name}".
-The full source code is freely available at:
+WARNING: This is a compressed version of "${pkg?.name}". Full source code is freely available at:
 ${pkg?.repository?.url || '---'}
 `;
 
